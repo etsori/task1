da182b2ab6a620bf5495b86216f0ab0dac69bb3a
Цориева ЦКБ-142 <tsorievaeva@yandex.ru>
2026-09-17 10:34:51 +0300

    Update 1.1.c

--- 1.1.c
+++ 1.1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double x = 1.7;
+    double y = 1.08;
+    double z = 0.5;
+
+    double a = sin(x) / sqrt(y * y + pow(sin(x), 2))
+             - x * x * y * z;
+
+    double b = pow(M_E, -z*x) * sqrt(x + 1) + pow(M_E, -y*x);
+    printf("a = %f\n", a);
+    printf("b = %f\n", b);
+
+    return 0;
+
 
